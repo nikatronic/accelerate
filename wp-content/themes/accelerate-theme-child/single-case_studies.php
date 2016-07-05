83454d01916212654691124ed148ce2a18771e29
nikatronic <nika@nikatronic.de>
2016-07-05 23:16:54 +0200

    spint2

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -14,16 +14,52 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-			<article class="case study">
-			<php? the_content(); ?>
-			</article>
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
+<div id="primary" class="site-content">
+<div id="content" role="main">
 
-		</div><!-- #content -->
+	<?php while ( have_posts() ) : the_post();
+		$services = get_field('services');
+		$client = get_field('client');
+		$link = get_field('site_link');
+		$image_1 = get_field('image1');
+		$image_2 = get_field('image2');
+		$image_3 = get_field('image3');
+		$size = "full"; ?>
+	
+	<article class="case-study">
+	<aside class="case-study-sidebar">
+	<h2><?php the_title(); ?></h2>
+	<h5><?php echo $services; ?></h5>
+	<h6><?php echo $client; ?></h6>
+	
+<?php the_content(); ?>
+
+<p><strong><a href="<?php echo $link; ?>">Site Link</a></strong></p>
+
+	</aside>
+			
+	<div class="case-study-images">
+	<?php if($image_1) { ?>
+	<?php echo wp_get_attachment_image( $image_1, $size ); ?>
+	
+	<?php } ?>
+	<?php if($image_2) { ?>
+	<?php echo wp_get_attachment_image( $image_2, $size ); ?>
+	
+	<?php } ?>
+	<?php if($image_3) { ?>
+	<?php echo wp_get_attachment_image( $image_3, $size ); ?>
+
+	<?php } ?>
+	</div>
+			
+<article class="case study">
+</article>
+
+
+<?php endwhile; // end of the loop. ?>
+
+	</div><!-- #content -->
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
